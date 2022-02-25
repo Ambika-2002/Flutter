3e469224618519807e9db9786e45f5b4297feedc
Ambika-2002 <100412159+Ambika-2002@users.noreply.github.com>
2022-02-25 21:52:22 +0530

    Create Question3.dart

---
+++ Task1/Question3.dart
@@ -0,0 +1,19 @@
+void main() {
+  List<Object> str = ['hello','world'];
+  List<Object> sen = reverse(str);
+  print(sen);
+}
+
+String? reverse(String str) {
+  
+    int len = 2;                                       
+    String ans = "";
+    for (int i = 5; i >= 0; i--)
+    {
+        ans += str[i] + " ";
+    }
+ 
+      print(str{[0 ,1]});
+    
+ 
+}
