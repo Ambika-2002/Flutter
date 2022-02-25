fb9e13e54253947ecdf4b9e9b08271c9e21c32ac
Ambika-2002 <100412159+Ambika-2002@users.noreply.github.com>
2022-02-25 19:30:34 +0530

    Update Question1.dart

--- Task1/Question1.dart
+++ Task1/Question1.dart
@@ -1,1 +1,11 @@
+// WAP to check if a number is divisible by 5.
 
+void main()
+{
+  int num = 101;
+  if(num % 5 == 0)
+    print("$num is divisible by 5");
+  
+  else
+    print("$num is not divisible by 5");
+}
