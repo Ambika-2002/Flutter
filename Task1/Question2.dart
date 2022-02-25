1648c495a286cbbc9e3c8cebb0134ad64f0e0e7f
Ambika-2002 <100412159+Ambika-2002@users.noreply.github.com>
2022-02-25 19:44:57 +0530

    Create Question2.dart

---
+++ Task1/Question2.dart
@@ -0,0 +1,16 @@
+//WAP to check if a number is even or odd using functions.
+
+void main()
+{
+  int num;
+  even_odd(19);
+}
+
+void even_odd(int num)
+{
+  if(num % 2==0)
+    print("$num is even");
+  
+  else
+    print("$num is odd");
+}
