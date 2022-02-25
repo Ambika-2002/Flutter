428a25fd20c09eb264cbf61d56ce2eb68531f785
Glance-JJ <100403922+Glance-JJ@users.noreply.github.com>
2022-02-25 20:25:17 +0530

    Create question 4.dart

---
+++ Task1/question 4.dart
@@ -0,0 +1,12 @@
+void main() 
+{
+  addition(25,5);
+  subtraction(25,5);
+  multiplication(25,5);
+  division(25,5);
+}
+
+void addition(int a,int b)=>print("sum = ${a+b}\n");
+void subtraction(int a,int b)=>print("difference = ${a-b}\n");
+void multiplication(int a,int b)=>print("multiplication = ${a*b}\n"); 
+void division(int a,int b)=>print("division = ${a/b}\n");
