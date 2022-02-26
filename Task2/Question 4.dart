6c93392ca34a5cecfb3d6033a54f14280064ba4c
Glance-JJ <100403922+Glance-JJ@users.noreply.github.com>
2022-02-26 21:40:46 +0530

    Create Question 4.dart

---
+++ Task2/Question 4.dart
@@ -0,0 +1,11 @@
+void main() 
+{ 
+   List<int> list=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+   map_alph(list);
+} 
+ 
+void map_alph(List list)
+{
+   Map<int,Object> alphabet={ list[0]:'a', list[1]:'b', list[2]:'c', list[3]:'d', list[4]:'e', list[5]:'f', list[6]:'g', list[7]:'h', list[8]:'i', list[9]:'j', list[10]:'k', list[11]:'l', list[12]:'m', list[13]:'n', list[14]:'o', list[15]:'p', list[16]:'q', list[17]:'r', list[18]:'s', list[19]:'t', list[20]:'u', list[21]:'v', list[22]:'w', list[23]:'x', list[24]:'y', list[25]:'z' }; 
+   print("$alphabet");
+}
