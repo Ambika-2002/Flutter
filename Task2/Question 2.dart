618bfcf50f69648a6ca52f9f877907cb29c4dd84
Glance-JJ <100403922+Glance-JJ@users.noreply.github.com>
2022-02-26 21:11:15 +0530

    Update Question 2.dart

--- Task2/Question 2.dart
+++ Task2/Question 2.dart
@@ -1,1 +1,14 @@
 
+void replace(List a)   //Function to give new list containing even elements
+{
+  int i;
+  int l=a.length;
+  for(i=0;i<l;i++)
+  {
+    if(a[i]%2!=0)
+    {
+      a.removeAt(i);
+    }
+  }
+  print("$a");
+}
