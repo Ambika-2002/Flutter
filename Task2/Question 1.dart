85be95735587889b9216a46e3fc03ee4e154ba96
Glance-JJ <100403922+Glance-JJ@users.noreply.github.com>
2022-02-26 20:17:06 +0530

    Create Question 1.dart

---
+++ Task2/Question 1.dart
@@ -0,0 +1,17 @@
+i)
+  b. int multiply(int a, int b) => return a * b;
+ii)
+  2.     Async functions 
+iii)
+  3. 	List
+iv)
+  3.     Error generated
+v)
+  3.    print(‘Your name is ‘{name}’); 
+vi)
+  2.	nums.forEach(print); 
+vii)
+  1.  Ensures type safety
+  2.  Reduce code duplication
+  4.  Can work in a single interface that takes a type parameter.
+
